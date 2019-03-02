4f548b2508b33f8d2f55709d231e407752b1ca92
Sourav Shrestha <shrestha.sourav30@gmail.com>
2019-03-02 23:11:24 +0530

    Connected App With Server With Response.

--- app/src/main/java/projects/imagescanner/MainActivity.java
+++ app/src/main/java/projects/imagescanner/MainActivity.java
@@ -1,27 +1,42 @@
 package projects.imagescanner;
-
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
-import java.text.SimpleDateFormat;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Integer> images = new ArrayList<>();
     ArrayList<String> dates = new ArrayList<>();
     ArrayList<RecentItems> recentItemsList = new ArrayList<>();
+
+    String baseUrl = "http://192.168.43.135:5000/";
+    String URL = "http://192.168.43.135/index.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
         myAdapterRecent = new MyAdapterRecent(recentItemsList);
         recentList.setAdapter(myAdapterRecent);
         digital = findViewById(R.id.digial);
+        handwritten = findViewById(R.id.hand);
         digital.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,20 +96,124 @@ public class MainActivity extends AppCompatActivity {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
                 Uri resultUri = result.getUri();
-                UploadPhoto(resultUri);
+                try {
+                    UploadImage(MediaStore.Images.Media.getBitmap(getContentResolver(), resultUri));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
             }
         }
     }
 
-    private void UploadPhoto(Uri resultUri) {
-        Toast.makeText(MainActivity.this,resultUri.toString(),Toast.LENGTH_SHORT).show();
+    public String getStringImage(Bitmap bm){
+        ByteArrayOutputStream ba = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG,100,ba);
+        byte[] imagebyte = ba.toByteArray();
+        String encode = Base64.encodeToString(imagebyte,Base64.DEFAULT);
+        return encode;
+    }
+
+    class CallPython extends AsyncTask{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            String jsonResponse = "";
+            HttpURLConnection urlConnection = null;
+            InputStream inputStream = null;
+            String url = Uri.parse(baseUrl).toString();
+            try {
+                urlConnection = (HttpURLConnection) new URL(url).openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setReadTimeout(10000 /* milliseconds */);
+                urlConnection.setConnectTimeout(15000 /* milliseconds */);
+                urlConnection.connect();
+                if (urlConnection.getResponseCode() == 200) {
+                    inputStream = urlConnection.getInputStream();
+                    jsonResponse = readFromStream(inputStream);
+                }
+                else
+                {
+                    Log.e("someError",Integer.toString(urlConnection.getResponseCode()));
+                }
+            }catch (IOException e) {
+                Log.e("someError","Exception occurred:",e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            Log.d("finalResult",jsonResponse);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+        }
+    }
+
+    @NonNull
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    private void UploadImage(final Bitmap bitmap) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                String s = response.trim();
+                if (!s.equalsIgnoreCase("Loi")) {
+                    Log.d("finalResult","eq");
+                    new CallPython().execute();
+                } else {
+                    Toast.makeText(MainActivity.this, "Failed To Upload !", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error + "", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                String image = getStringImage(bitmap);
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("IMG", image);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     public void addItems(String title,int image,String date) {
-        images.add(image);
-        titles.add(title);
-        dates.add(date);
+       images.add(image);
+       titles.add(title);
+       dates.add(date);
     }
 }
